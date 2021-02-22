a4d589679356c9d5e96954139b0a8b9f98fce023
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2021-02-22 22:01:49 +0200

    Add empty project

---
+++ src/main/java/world/ucode/pixelizator/PixelizatorApplication.java
@@ -0,0 +1,13 @@
+package world.ucode.pixelizator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelizatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelizatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/world/ucode/pixelizator/PixelizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package world.ucode.pixelizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
