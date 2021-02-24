b1f21195ee6376cc2076094e9f09183801200023
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2021-02-24 21:07:32 +0200

    Move DB creation to a separate file

--- src/main/java/world/ucode/pixelizator/PixelizatorApplication.java
+++ src/main/java/world/ucode/pixelizator/PixelizatorApplication.java
@@ -2,11 +2,13 @@ package world.ucode.pixelizator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import world.ucode.pixelizator.util.DBHelper;
 
 @SpringBootApplication
 public class PixelizatorApplication {
 
     public static void main(String[] args) {
+        new DBHelper().createDbIfNotExists();
         SpringApplication.run(PixelizatorApplication.class, args);
     }
 }
--- src/main/java/world/ucode/pixelizator/controller/HomeController.java
+++ src/main/java/world/ucode/pixelizator/controller/HomeController.java
@@ -19,89 +19,8 @@ import java.util.stream.Stream;
 @RestController
 public class HomeController {
 
-    public static final String DB_NAME = "songs";
-
     @GetMapping("/")
     String home() {
-
-        if (!dbExists())
-            crateDb();
-
         return "You are at Pixelizator home page";
-    }
-
-    private void crateDb() {
-        Connection conn = null;
-        try {
-            URL url = this.getClass().getClassLoader().getResource("data/init.sql");
-            var filePath = Paths.get(url.toURI()).toString();
-
-            conn = getConnection();
-            ScriptRunner sr = new ScriptRunner(conn);
-            Reader reader = new BufferedReader(new FileReader(filePath));
-            sr.runScript(reader);
-        } catch (SQLException | FileNotFoundException | URISyntaxException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            closeConnection(conn);
-        }
-    }
-
-    private boolean dbExists() {
-
-        var exists = false;
-
-        Connection conn = null;
-        try {
-            // db parameters
-            conn = getConnection();
-
-            var sql = "SHOW DATABASES";
-            Statement statement = conn.createStatement();
-            var rs = statement.executeQuery(sql);
-
-            while (rs.next()) {
-                var dbName = rs.getString(1);
-                if (dbName.equals(DB_NAME)) {
-                    exists = true;
-                    break;
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            closeConnection(conn);
-        }
-
-        return exists;
-    }
-
-    private Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/";
-        String user = "root";
-        String password = "";
-        return DriverManager.getConnection(url, user, password);
-    }
-
-    private void closeConnection(Connection connection) {
-        try {
-            if (connection != null)
-                connection.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    private String fileContent(URL url) throws URISyntaxException {
-        var filePath = url.toURI();
-        StringBuilder contentBuilder = new StringBuilder();
-
-        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return contentBuilder.toString();
     }
 }
